43a2476a661d037f98be008e40de9fc1deed39ea
Jonathan Estrella <jestrella04@gmail.com>
2020-04-13 16:45:53 -0400

    Adding plain identifier test cases

---
+++ simplewebpaytest/html/swp-019.php
@@ -0,0 +1,38 @@
+<script type="text/javascript">
+	var params = "";
+	
+	params += "merchantid=12721627";
+	params += "&email=cenposqatest@gmail.com";
+	params += "&customercode=swp019";
+	params += "&address=9048";
+	params += "&zipcode=33189";
+	params += "&isemail=false";
+	params += "&iscvv=false";
+	
+	$(document).ready(function()
+	{
+		$("#NewCenposPlugin").createWebpay(
+		{
+			url: '<?php echo $webpay_path ?>',
+			params: params,
+			width: "500",
+			height: "450",
+			sessionToken: false,
+			success: formatResponse,
+			cancel: formatResponse
+		});
+		
+		$("#submit").on('click', function() 
+		{
+			$("#NewCenposPlugin").submitAction();
+		});
+	});
+
+	function formatResponse(data)
+	{
+		var str = '<pre><code>' + JSON.stringify(data, undefined, 2) + '</code></pre>';
+
+		top.$('#webpay-response').empty();
+		top.$('#webpay-response').append(str);
+	}
+</script>
