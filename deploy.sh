a5fe9f097f74dbe3c3df9eaa33ad3345339ddfdb
Jonathan Estrella <jestrella04@gmail.com>
2021-05-12 17:54:22 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,18 @@
+#!/bin/bash
+
+# Create the log directory and update permissions
+mkdir -m777 tools/integrator/getresponse/log/
+
+# Pull the latest changes from the git repository
+# git reset --hard
+# git clean -df
 git pull origin master
+
+# Install/update composer dependecies
+#composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+# Install node modules
+npm install
+
+# Build assets with Webpack
 npm run production
